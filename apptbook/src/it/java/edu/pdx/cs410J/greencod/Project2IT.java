ddddd98988681ffa7c6240c85ff9163a074aaf52
Cody-Green-School <81730266+Cody-Green-School@users.noreply.github.com>
2021-07-13 13:35:27 -0700

    Changed files to project2

--- apptbook/src/it/java/edu/pdx/cs410J/greencod/Project2IT.java
+++ apptbook/src/it/java/edu/pdx/cs410J/greencod/Project2IT.java
@@ -1,4 +1,165 @@
 package edu.pdx.cs410J.greencod;
 
-public class Project2IT {
+import edu.pdx.cs410J.InvokeMainTestCase;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Integration tests for the {@link Project2} main class.
+ */
+class Project2IT extends InvokeMainTestCase {
+
+
+  /**
+   * Invokes the main method of {@link Project2} with the given arguments.
+   */
+  private MainMethodResult invokeMain(String... args) {
+    return invokeMain( Project2.class, args );
+  }
+
+
+  /**
+   * Tests that invoking the main method with no arguments issues an error
+   */
+  @Test
+  void testNoCommandLineArguments() {
+    MainMethodResult result = invokeMain();
+    assertThat(result.getExitCode(), equalTo(1));
+    assertThat(result.getTextWrittenToStandardError(), containsString(Project2.MISSING_COMMAND_LINE_ARGUMENTS));
+  }
+  /**
+   * Tests that invoking the main method with no arguments issues an error and shows usage message
+   */
+  @Test
+  void testNoCommandArgsPrintsMissingArgs(){
+    MainMethodResult result = invokeMain();
+    assertThat(result.getTextWrittenToStandardError(), containsString(Project2.MISSING_COMMAND_LINE_ARGUMENTS));
+    assertThat(result.getTextWrittenToStandardError(), containsString(Project2.USAGE_MESSAGE));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+  /**
+   * Tests that invoking the main method with no description issues error
+   */
+  @Test
+  void testMissingDescription() {
+    MainMethodResult result = invokeMain(Project2.class, "Cody");
+    assertThat(result.getTextWrittenToStandardError(), containsString(Project2.MISSING_DESCRIPTION));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+  /**
+   * Tests that invoking the main method with no begin date issues error
+   */
+  @Test
+  void testMissingBeginDate() {
+    MainMethodResult result = invokeMain(Project2.class, "Cody", "Head Transplant Consultation");
+    assertThat(result.getTextWrittenToStandardError(), containsString(Project2.MISSING_BEGIN_DATE));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+  /**
+   * Tests that invoking the main method with no begin time issues error
+   */
+  @Test
+  void testMissingBeginTime() {
+    MainMethodResult result = invokeMain(Project2.class, "Cody", "Head Transplant Consultation", "07/21/3000");
+    assertThat(result.getTextWrittenToStandardError(), containsString(Project2.MISSING_BEGIN_TIME));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+  /**
+   * Tests that invoking the main method with no end date issues error
+   */
+  @Test
+  void testMissingEndDate() {
+    MainMethodResult result = invokeMain(Project2.class, "Cody", "Head Transplant Consultation","07/21/3000", "11:11");
+    assertThat(result.getTextWrittenToStandardError(), containsString(Project2.MISSING_END_DATE));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+  /**
+   * Tests that invoking the main method with no end time issues error
+   */
+  @Test
+  void testMissingEndTime() {
+    MainMethodResult result = invokeMain(Project2.class, "Cody", "Head Transplant Consultation","07/21/3000", "11:11", "07/21/1992");
+    assertThat(result.getTextWrittenToStandardError(), containsString(Project2.MISSING_END_TIME));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+  /**
+   * Tests that invoking the main method with a begin time out of range issues error
+   */
+  @Test
+  void testBeginTimeInRange() {
+    MainMethodResult result = invokeMain(Project2.class, "Cody", "Head Transplant Consultation","07/21/3000", "11:99", "07/21/1992", "11:11");
+    assertThat(result.getTextWrittenToStandardError(), containsString(Project2.TIME_NOT_CORRECT));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+  /**
+   * Tests that invoking the main method with an end time out of range issues error
+   */
+  @Test
+  void testEndTimeInRange() {
+    MainMethodResult result = invokeMain(Project2.class, "Cody", "Head Transplant Consultation","07/21/3000", "11:00", "07/21/1992", "11:99");
+    assertThat(result.getTextWrittenToStandardError(), containsString(Project2.TIME_NOT_CORRECT));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+  /**
+   * Tests that invoking the main method with an end time using characters fails
+   */
+  @Test
+  void testTimeIsNumber() {
+    MainMethodResult result = invokeMain(Project2.class, "Cody", "Head Transplant Consultation","07/21/3000", "11:xx", "07/21/1992", "11:99");
+    assertThat(result.getTextWrittenToStandardError(), containsString(Project2.TIME_NOT_CORRECT));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+  /**
+   * Tests that invoking the main method with a month out of range issues error
+   */
+  @Test
+  void testDateMonthInRange() {
+    MainMethodResult result = invokeMain(Project2.class, "Cody", "Head Transplant Consultation","15/30/3000", "11:00", "07/21/1992", "11:11");
+    assertThat(result.getTextWrittenToStandardError(), containsString(Project2.DATE_NOT_CORRECT));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+  /**
+   * Tests that invoking the main method with a day out of range issues error
+   */
+  @Test
+  void testDateDayInRange() {
+    MainMethodResult result = invokeMain(Project2.class, "Cody", "Head Transplant Consultation","12/32/3000", "11:00", "07/21/1992", "11:11");
+    assertThat(result.getTextWrittenToStandardError(), containsString(Project2.DATE_NOT_CORRECT));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+  /**
+   * Tests that invoking the main method with a year out of range issues error
+   */
+  @Test
+  void testDateYearInRange() {
+    MainMethodResult result = invokeMain(Project2.class, "Cody", "Head Transplant Consultation","12/31/300", "11:00", "07/21/1992", "11:11");
+    assertThat(result.getTextWrittenToStandardError(), containsString(Project2.DATE_NOT_CORRECT));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+  /**
+   * Tests that invoking the main method with a character issues error
+   */
+  @Test
+  void testDateIsNums() {
+    MainMethodResult result = invokeMain(Project2.class, "Cody", "Head Transplant Consultation","12/31/CODY", "11:00", "07/21/1992", "11:11");
+    assertThat(result.getTextWrittenToStandardError(), containsString(Project2.DATE_NOT_CORRECT));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+  /**
+   * Tests that -print works
+   */
+  @Test
+  void testPrint() {
+    MainMethodResult result = invokeMain(Project2.class, "-print", "Cody", "Head Transplant Consultation","12/31/3000", "11:00", "07/21/1992", "11:11");
+    assertThat(result.getTextWrittenToStandardOut(), containsString(("Head Transplant Consultation from 12/31/3000 11:00 until 07/21/1992 11:11")));
+  }
+  @Test
+  void testReadMe() {
+    MainMethodResult result = invokeMain(Project2.class, "-README", "Cody", "Head Transplant Consultation","12/31/3000", "11:00", "07/21/1992", "11:11");
+    assertThat(result.getTextWrittenToStandardOut(), containsString(("This is a README file for Project 1! This program reads in arguments to create an appointment, then adds those appointments to an appointment book class that will be updated in later projects.")));
+  }
+
 }
