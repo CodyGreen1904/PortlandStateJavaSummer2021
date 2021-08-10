c6a002d4835e16869d5d004b4d7662646be88be5
Cody-Green-School <81730266+Cody-Green-School@users.noreply.github.com>
2021-08-09 19:23:19 -0700

    Updated make appointment page

--- apptbook-android/app/src/main/java/edu/pdx/cs410j/greencod/AppointmentBookMenu.java
+++ apptbook-android/app/src/main/java/edu/pdx/cs410j/greencod/AppointmentBookMenu.java
@@ -2,7 +2,10 @@ package edu.pdx.cs410j.greencod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AppointmentBookMenu extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class AppointmentBookMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment_book_menu);
+
+        Button makeAppointmentButton = findViewById(R.id.makeAppointmentButton);
+        makeAppointmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AppointmentBookMenu.this, MakeAppointment.class);
+                startActivity(intent);
+            }
+        });
     }
 }
