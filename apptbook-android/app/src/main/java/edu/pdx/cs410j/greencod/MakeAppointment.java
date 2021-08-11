68e6336e012401c627900ebc552bbf9366906ff0
Cody-Green-School <81730266+Cody-Green-School@users.noreply.github.com>
2021-08-10 18:21:12 -0700

    Started making methods to verify inputs

--- apptbook-android/app/src/main/java/edu/pdx/cs410j/greencod/MakeAppointment.java
+++ apptbook-android/app/src/main/java/edu/pdx/cs410j/greencod/MakeAppointment.java
@@ -3,6 +3,8 @@ package edu.pdx.cs410j.greencod;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MakeAppointment extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MakeAppointment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_appointment);
+
+        Button submit = findViewById(R.id.makeAppointmentButton);
+        submit.setOnClickListener(v -> verifyValuesAndMakeAppointment());
+    }
+
+    private void verifyValuesAndMakeAppointment() {
+        
     }
 }
